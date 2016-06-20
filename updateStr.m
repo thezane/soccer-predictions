e4df9ef853ef868d14d81ad0192478d9565b97e6
Huang Zao <g6zane@cdf.toronto.edu>
2016-06-20 03:33:13 -0400

    simplified ranking

--- updateStr.m
+++ updateStr.m
@@ -1,35 +1,22 @@
 function [a d] = updateStr(A, a, d, alpha)
   aPrev = a;
   dPrev = d;
-  tol = 0.01;
-  
-  while (1)
-    [aNext dNext] = computeStr(A, a, d);
-    aDel = aNext - a;
-    dDel = dNext - d;
-    
-    if (norm(aDel) < tol && norm(dDel) < tol)
-      strNext = [aNext dNext];
-      str = [aPrev dPrev];
-      str = alpha .* strNext + (1 - alpha) .* str;
-      a = str(1: 2, 1);
-      d = str(1: 2, 2);
-      [a d]
-      return
-    end
-    
-    a = aNext;
-    d = dNext;
-  end
+  [aNext dNext] = computeStr(A, a, d);
+  strNext = [aNext dNext];
+  str = [aPrev dPrev];
+  str = alpha .* strNext + (1 - alpha) .* str;
+  a = str(:, 1);
+  d = str(:, 2);
+  [a d]'
 end
 
 function [a d] = computeStr(A, a, d)
   tol = 1e-06;
-  lambda = 0.5;
-  A = A + lambda - lambda * eye(2);
+  lambda = 0.01;
+  A = A + lambda * fliplr(eye(2));
+  pause
   [aRelA dRelA] = computeStrRelA(A, a, tol);
   [aRelD dRelD] = computeStrRelD(A, d, tol);
-  onesCol = ones(2, 1);
   aNext = (aRelA + aRelD) / 2;
   dNext = (dRelA + dRelD) / 2;
   a = aNext;
