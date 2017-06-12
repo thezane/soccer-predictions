5461174413f75d125368100a7c24d7b79f9611fe
Z <noneofyourbusiness.com>
2017-06-11 21:04:56 -0400

    data

--- odmsRater/objects/RatingsOutput.R
+++ odmsRater/objects/RatingsOutput.R
@@ -34,12 +34,11 @@ updateStrMeanCosts.RatingsOutput <- function(rOutput, dataset) {
 }
 
 updateTotalCosts <- function(rOutput, rOptions) {
-  print(rOutput$costs)
+  browser()
   rOutput$costs["training"] <- updateTotalCost(
       rOutput$costs[["training"]], rOptions)
   rOutput$costs["validation"] <- updateTotalCost(
       rOutput$costs[["validation"]], rOptions)
-  print(rOutput$costs)
   rOutput
 }
 
