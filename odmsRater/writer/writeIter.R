f94928289fc593d62362d5a53626e6db6fb6359b
Z <noneofyourbusiness.com>
2017-06-11 23:32:47 -0400

    data

--- odmsRater/writer/writeIter.R
+++ odmsRater/writer/writeIter.R
@@ -1,6 +1,6 @@
 writeIter <- function(y, x, dataPath) {
   iterData <- matrix(c(y, x), nrow=1)
   outFile <- paste(dataPath, "odms-iters", sep="")
-  write.table(iterData, paste(outFile, ".csv", sep=""), append=TRUE,
+  write.table(iterData, paste(outFile, ".csv", sep=","), append=TRUE,
       col.names=FALSE, row.names=FALSE)
 }
