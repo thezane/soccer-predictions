1b81e6adeae48be2e78762ea901521b2c8c35707
Z <noneofyourbusiness.com>
2017-06-18 01:42:09 -0400

    data

--- odmsRater/main/optimizeRNN.R
+++ odmsRater/main/optimizeRNN.R
@@ -76,6 +76,12 @@ updateRNN <- function(x, rData) {
 
 computeGradientPar <- function(x, n, f, e, cluster) {
   I <- c(1: n)
-  g <- 0 * I
+  g <- parSapply(cluster, I, function(i, f.=f, x.=x, e.=e) {
+      xForDiff <- x
+      xForDiff[i] <- xForDiff[i] + e
+      xBackDiff <- x
+      xBackDiff[i] <- xBackDiff[i] - e
+      y <- (f(xForDiff) - f(xBackDiff)) / (2 * e)
+      y})
   g
 }
