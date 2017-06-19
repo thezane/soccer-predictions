b6ee62237dcb506ec44524f53d712c7099591d22
Z <noneofyourbusiness.com>
2017-06-18 23:40:07 -0400

    data

--- odmsRater/main/computeLayerRatings.R
+++ odmsRater/main/computeLayerRatings.R
@@ -1,6 +1,6 @@
 computeLayerRatings <- function(game, rOptions, homeTeam, awayTeam,
     strPostNorm) {
-  alphas <- game$weight * rOptions$k * game$reliability
+  alphas <- game$reliability * game$weight * rOptions$k
   strNextNorm <- alphas * strPostNorm + (1 - alphas) * game$strNorm
   strNextNorm
 }
