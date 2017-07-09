af4cfd58017811600ede447247564b76c75e6f05
Z <noneofyourbusiness.com>
2017-07-09 11:02:39 -0400

    data

--- odmsRater/objects/RatingsOptions.R
+++ odmsRater/objects/RatingsOptions.R
@@ -17,7 +17,7 @@ newRatingsOptions <- function(fTree) {
     corrBeta=-1,
     theta=1,
     tieBias=-3.2,
-    tieBeta=-8,
+    tieBeta=-4,
 
     # Default federation strengths for Africa, Asia, Europe,
     # North America, Oceania and South America respectively
