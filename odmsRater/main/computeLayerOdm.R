c02af08d9e57890446b69a972fb8cc8fc3dc40cc
Z <noneofyourbusiness.com>
2017-07-29 02:03:48 -0400

    Revert "data"
    This reverts commit 4225bc04e79daab3c11af2bb7595de97883f3169.

--- odmsRater/main/computeLayerOdm.R
+++ odmsRater/main/computeLayerOdm.R
@@ -1,14 +1,34 @@
-computeLayerOdm <- function(A, strNorm, b, c) {
+computeLayerOdm <- function(A, strNorm, b, c, odmIter) {
   teamStr <- exp(strNorm)
   a <- teamStr[, 1];
   d <- teamStr[, 2];
-  teamStrFlipped <- matrix(c(a[1], a[2], d[2], d[1]), 2, 2)
   A <- b * A + c;
-  strPostFlipped <- A %*% (1 / teamStrFlipped)
-  aPostFlipped <- strPostFlipped[, 1]
-  dPostFlipped <- strPostFlipped[, 2]
-  strPost <- matrix(c(aPostFlipped[2], aPostFlipped[1],
-      dPostFlipped[1], dPostFlipped[2]), 2, 2) 
+  i <- 1
+
+  while (i <= odmIter) {
+    strPost <- computeAD(A, a, d, odmIter);
+    aPost <- strPost[, 1]
+    dPost <- strPost[, 2]
+    aDel <- matrix(aPost - a)
+    dDel <- matrix(dPost - d)
+    a <- aPost
+    d <- dPost
+    i <- i + 1
+  }
+  
   strPostNorm <- log(strPost)
   strPostNorm
+}
+
+computeAD <- function(A, a, d, tolScale) {
+  strRelA = scaleRating(A, a, tolScale);
+  aRelA <- strRelA[["x"]]
+  dRelA <- strRelA[["y"]]
+  strRelD <- scaleRating(t(A), d, tolScale);
+  dRelD <- strRelD[["x"]]
+  aRelD <- strRelD[["y"]]
+  aPost <- (aRelA + aRelD) / 2;
+  dPost <- (dRelA + dRelD) / 2;
+  strPost <- matrix(c(aPost, dPost), 2, 2)
+  strPost
 }
