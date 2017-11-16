7b460c94f8ea44b6b1b5b52efa496eb9fee8563d
dustin <dustin.wehr@gmail.com>
2017-11-16 10:58:44 -0500

    output AwayWin and Tie also. check that predictions sum to close to 1.

--- analysis/combine_predictions.py
+++ analysis/combine_predictions.py
@@ -1,7 +1,8 @@
 import csv
+from decimal import *
 
 IN_ROOT = "../output/"
-in_file_names = (
+IN_FILE_NAMES = (
 "odms-matches-bivpois-odmiter1.csv",
 "odms-matches-bivpois.csv",
 "odms-matches-odmiter1.csv",
@@ -16,33 +17,45 @@ in_file_names = (
 )
 
 OUT_ROOT = "./output/"
+CSV_HEADER = ("HomeTeam","AwayTeam","Date","Contest","HomeGoals","AwayGoals","HomeAdvantage","HomeStrAgg","AwayStrAgg","HomeStrAggNext","AwayStrAggNext","HomeWin","Tie","AwayWin","SSE")
 
-csv_header = ("HomeTeam","AwayTeam","Date","Contest","HomeGoals","AwayGoals","HomeAdvantage","HomeStrAgg","AwayStrAgg","HomeStrAggNext","AwayStrAggNext","HomeWin","Tie","AwayWin","SSE")
-row_len = len(csv_header)
-m = {csv_header[i]:i for i in range(len(csv_header))}
+COLUMNS_TO_COLLECT = (
+	"HomeWin",
+	"Tie",
+	"AwayWin"
+)
 
-homewin = []
+# "HomeTeam":0, "AwayTeam":1, etc
+M = {CSV_HEADER[i]:i for i in range(len(CSV_HEADER))}
 
-for filenum in range(len(in_file_names)):
-	filename = in_file_names[filenum]
+outputs = {colname:[] for colname in COLUMNS_TO_COLLECT}
+
+for filenum in range(len(IN_FILE_NAMES)):
+	filename = IN_FILE_NAMES[filenum]
 	with open(IN_ROOT + filename,'r', encoding='windows-1252') as csvfile:
 		reader = csv.reader(csvfile, delimiter=',', quotechar='"')
 		this_csv_header = None
 		datarow_ind = 0
 		for row in reader:
-			assert len(row) == row_len
+			assert len(row) == len(CSV_HEADER)
 			if not this_csv_header:
 				# just to check that they all have the expected header
 				this_csv_header = row
-				assert tuple(this_csv_header) == csv_header
+				assert tuple(this_csv_header) == CSV_HEADER
 			else:
-				if datarow_ind > len(homewin) - 1:
-					homewin.append([])
-				homewin[datarow_ind].append(row[m["HomeWin"]])
+				for col in COLUMNS_TO_COLLECT:					
+					if datarow_ind > len(outputs[col]) - 1:
+						outputs[col].append([])
+					outputs[col][datarow_ind].append(row[M[col]])
+
+				predict_sum = sum(Decimal(outputs[col][datarow_ind][-1]) for col in COLUMNS_TO_COLLECT)
+				assert abs(1 - predict_sum) <= .001, str(abs(1 - predict_sum)) + " " + str([outputs[col][datarow_ind][-1] for col in COLUMNS_TO_COLLECT])
+
 				datarow_ind += 1
 
-	with open(OUT_ROOT + "HomeWin.csv", 'w', encoding='windows-1252') as homewin_out_file:
-		homewin_out_file.writelines((",".join(row) + "\n" for row in homewin))
+	for col in COLUMNS_TO_COLLECT:
+		with open(OUT_ROOT + col + ".csv", 'w', encoding='windows-1252') as out_file:
+			out_file.writelines((",".join(row) + "\n" for row in outputs[col]))
 
 
 
