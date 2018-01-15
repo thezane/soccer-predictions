a022c613d6a6e689a7b362856cd17aa2b993fd2e
Z <noneofyourbusiness.com>
2018-01-14 23:44:42 -0500

    Prepare retraining

--- odmsRater/hindcastAll.R
+++ odmsRater/hindcastAll.R
@@ -1,8 +1,10 @@
-source("hindcastGames.R")
-OPTIONS_DIR <- "options"
-options_files <- list.files(OPTIONS_DIR)
+hindcastAll <- function() {
+  source("hindcastGames.R")
+  OPTIONS_DIR <- "options"
+  options_files <- list.files(OPTIONS_DIR)
 
-for (options_file in options_files) {
-  rOptions <- source(paste(OPTIONS_DIR, "/", options_file, sep=""))
-  rData <- hindcastGames(rOptions)
+  for (options_file in options_files) {
+    rOptions <- source(paste(OPTIONS_DIR, "/", options_file, sep=""))
+    rData <- hindcastGames(rOptions)
+  }
 }
--- odmsRater/hindcastGames.R
+++ odmsRater/hindcastGames.R
@@ -9,22 +9,22 @@ hindcastGames <- function(rOptions) {
   srcFiles <- list.files(".", regexRFiles,
       full.names=TRUE, recursive=TRUE)
   sapply(srcFiles, source)
-  #readsData <- readData(rOptions, inputPath)
-  #tTree <- readsData[["tTree"]]
-  #gTree <- readsData[["gTree"]]
+  readsData <- readData(rOptions, inputPath)
+  tTree <- readsData[["tTree"]]
+  gTree <- readsData[["gTree"]]
   T <- readsData[["T"]]
-  #gi <- new.EventIterator(gTree)
-  #rOutput <- new.RatingsOutput(tTree, gTree, gi)
+  gi <- new.EventIterator(gTree)
+  rOutput <- new.RatingsOutput(tTree, gTree, gi)
   
-  #if (rOptions$isOptimized) {
-  #	rOutput <- computeRNN(rOptions, rOutput)
-  #  rData <- list(rOptions=rOptions, rOutput=rOutput)
-  #}
-  #else {
-  #  rData <- list(rOptions=rOptions, rOutput=rOutput)
-  #  rData <- optimizeRNN(tTree, gTree, gi, rData, outputPath)
-  #}
+  if (rOptions$isOptimized) {
+  	rOutput <- computeRNN(rOptions, rOutput)
+    rData <- list(rOptions=rOptions, rOutput=rOutput)
+  }
+  else {
+    rData <- list(rOptions=rOptions, rOutput=rOutput)
+    rData <- optimizeRNN(tTree, gTree, gi, rData, outputPath)
+  }
     
-  #writeGames(rData, T, outputPath)
-  #rData
+  writeGames(rData, T, outputPath)
+  rData
 }
