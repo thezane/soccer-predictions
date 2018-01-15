9952fc95a3c6bc6a8b84b9b81f5b3c56cbb3a29f
Z <noneofyourbusiness.com>
2018-01-15 00:41:55 -0500

    Prepare retraining

--- odmsRater/hindcastAll.R
+++ odmsRater/hindcastAll.R
@@ -1,16 +1,17 @@
 hindcastAll <- function() {
+  ENV <- globalenv()
   COMPUTERS_DIR <- "computers"
   OPTIONS_DIR <- "options"
   computers_files <- list.files(COMPUTERS_DIR)
   options_files <- list.files(OPTIONS_DIR)
   sourceFiles(list.files(COMPUTERS_DIR), COMPUTERS_DIR)
   sourceFiles(list.files(OPTIONS_DIR),OPTIONS_DIR)
-  function_names <- ls(globalenv())
+  function_names <- ls(ENV)
   options_constructor_names <- function_names[sapply(
       function_names,
       function(function_name) {
       startsWith(function_name, "new.RatingsOptions")})]
-  source("hindcastGames.R", local=globalenv())
+  source("hindcastGames.R", local=ENV)
 
   for (options_constructor_name in options_constructor_names) {
     options_constructor <- get(options_constructor_name)
@@ -21,6 +22,6 @@ hindcastAll <- function() {
 sourceFiles <- function(files, dir) {
   for (file in files) {
     file_full <- paste(dir, "/", file, sep="")
-    source(file_full, local=globalenv())
+    source(file_full, local=ENV)
   }
 }
